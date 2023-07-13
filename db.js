91ae94acc512e119847d75e00867cae9781e7775
yashweblife <yashweblife20@gmail.com>
2023-07-13 18:57:01 -0500

    primary setup

--- db.js
+++ db.js
@@ -7,3 +7,31 @@ const mongoOptions = { useNewUrlParser: true };
 const state = {
     db: null
 }
+
+const connect = (cb) => {
+    if (state.db) {
+        cb();
+    } else {
+        MongoClient.connect(url, mongoOptions, (err, client) => {
+            if (err) {
+                cb(err)
+            } else {
+                state.db = client.db(dbname)
+                cb()
+            }
+        })
+    }
+}
+
+const getPrimaryKey = (_id) => {
+    return (ObjectId(_id))
+}
+const getDB = () => {
+    return state.db
+}
+
+module.exports = {
+    getDB,
+    connect,
+    getPrimaryKey
+}
