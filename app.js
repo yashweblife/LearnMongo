9496fb4efa94f1435b28a14d2918421d61046966
yashweblife <yashweblife20@gmail.com>
2023-07-13 18:58:53 -0500

    add primary app code

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const db = require('./db');
 app.use(bodyParser.json());
 
 const collection = "todo";
+db.connect((err)=>{
+    if(err){
+        console.log("Error Connecting To DB")
+        process.exit(1)
+    }else{
+        app.listen(3000,()=>{
+            console.log("Connected to DB at 3000")
+        })
+    }
+})
